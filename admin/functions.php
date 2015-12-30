cf906a65170477e61c7c2b264d591d8fef88fde1
sem schilder <sem.schilder@gmail.com>
2015-12-30 12:59:41 +0100

    user edit It is now possible to edit user info in the admin. Need to port this to user front-end

--- admin/functions.php
+++ admin/functions.php
@@ -31,6 +31,64 @@ function getAllUsers(){
 	return $sth->fetchAll();
 }
 
+function updateUser($userInfo){
+	global $dbh;
+	foreach($userInfo['meta_key'] as $key => $value){
+		$query  =  $dbh->query("SELECT meta_value FROM invoice_usermeta WHERE meta_key='$key' AND user_id='{$userInfo['id']}'");
+	    $q      =  $query->fetch(PDO::FETCH_OBJ);
+	    if(isset($q->meta_value)){
+	        $sth = $dbh->prepare("UPDATE invoice_usermeta SET meta_value=:meta_value WHERE meta_key=:meta_key AND user_id=:userid;");
+	        $sth->bindParam(':meta_key', $key, PDO::PARAM_INT);
+			$sth->bindParam(':meta_value', $value, PDO::PARAM_INT);
+			$sth->bindParam(':userid', $userInfo['id'], PDO::PARAM_INT);
+			$sth->execute();
+	    }else{
+		    $sth = $dbh->prepare("INSERT INTO meta_value(user_id, ,meta_key, meta_value) VALUES(:userid, :meta_value, :meta_key)");
+	        $sth->bindParam(':meta_key', $key, PDO::PARAM_INT);
+			$sth->bindParam(':meta_value', $value, PDO::PARAM_INT);
+			$sth->bindParam(':userid', $userInfo['id'], PDO::PARAM_INT);
+			$sth->execute();
+	    }
+    }
+}
+
+function getUserData($custnum){
+	global $dbh;
+	$sth = $dbh->prepare("
+	SELECT u.*, m1.meta_value, m2.meta_value, m3.meta_value, m4.meta_value, m5.meta_value, m6.meta_value
+	FROM invoice_users u
+	LEFT OUTER JOIN invoice_usermeta m1
+	    on u.id = m1.user_id 
+	    and m1.meta_key='adress_street'
+	LEFT OUTER JOIN invoice_usermeta m2
+	    on u.id = m2.user_id 
+	    and m2.meta_key='adress_zip'
+	LEFT OUTER JOIN invoice_usermeta m3
+	    on u.id = m3.user_id 
+	    and m3.meta_key='adress_city'
+	LEFT OUTER JOIN invoice_usermeta m4
+	    on u.id = m4.user_id 
+	    and m4.meta_key='adress_country'
+	LEFT OUTER JOIN invoice_usermeta m5
+	    on u.id = m5.user_id 
+	    and m5.meta_key='user_phone'
+	LEFT OUTER JOIN invoice_usermeta m6
+	    on u.id = m6.user_id 
+	    and m6.meta_key='full_name'
+	WHERE usercustnum=:customer");
+	$sth->bindParam(':customer', $custnum, PDO::PARAM_INT);
+	$sth->execute();
+	return $sth->fetch();
+}
+
 if (!isset($_SESSION['user']) && $_SERVER['SCRIPT_NAME'] != '/login.php'){
 	header("Location: {$domain}/login/?red=".urlencode($_SERVER['REQUEST_URI']));
+}
+
+if ($_SESSION['user_priv'] != '0'){
+	die('Not Allowed :(');
+}
+
+if (isset($_POST['userdatasubmit'])) {
+	updateUser($_POST);
 }
--- admin/users.php
+++ admin/users.php
@@ -23,8 +23,8 @@
 				                        foreach (getAllUsers() as $user){ 
 				                        ?>
 			                        <tr>
-										<th><a class='invoice number' href='/user/<?php echo $user['usercustnum'] ?>'><?php echo $user['usercustnum'] ?></a></th>
-										<th><a class='invoice name' href='/user/<?php echo $user['usercustnum'] ?>'><?php echo $user[8] ?></a></th>
+										<th><a class='invoice number' href='<?php echo $user['usercustnum'] ?>'><?php echo $user['usercustnum'] ?></a></th>
+										<th><a class='invoice name' href='<?php echo $user['usercustnum'] ?>'><?php echo $user[8] ?></a></th>
 										<th><a class='invoice email' href='mailto:<?php echo $user['user_email'] ?>'><?php echo $user['user_email'] ?></a></th>
 										<th><a class='invoice phone' href='tel:<?php echo $user[7] ?>'><?php echo $user[7] ?></a></th>
 										<th><a class='invoice edit' href='<?php echo $user['usercustnum'] ?>'><i class="fa fa-pencil-square-o"></i></a></th>
@@ -39,10 +39,11 @@
             </div>
             <?php
 	        }else{
+		        $userdata = getUserData($_GET['id']);
 		        ?>
 		    <div class="container">
             	<div class="row">
-	            	<form>
+	            	<form method="post">
 		            	<div class="eight columns">
 			            	<div class="element">
 				            	<h5>Account gegevens</h5>
@@ -50,49 +51,51 @@
 								  <div class="row">
 								    <div class="six columns">
 								    	<label for="userdetails-name">Naam</label>
-										<input class="u-full-width" type="text" placeholder="Uw naam" id="userdetails-name" name="userdetails[name]">
+										<input class="u-full-width" type="text" placeholder="Uw naam" id="userdetails-name" name="meta_key[full_name]" value="<?php echo $userdata[12] ?>">
 										<label for="userdetails-email">E-mail</label>
-										<input class="u-full-width" type="email" placeholder="Uw e-mail adres" id="userdetails-email" name="userdetails[email]">
+										<input class="u-full-width" type="email" placeholder="Uw e-mail adres" id="userdetails-email" name="meta_key[user_email]" value="<?php echo $userdata['user_email'] ?>">
 										<label for="userdetails-phone">Telefoon</label>
-										<input class="u-full-width" type="tel" placeholder="Uw Telefoonnummer" id="userdetails-phone" name="userdetails[phone]">
+										<input class="u-full-width" type="tel" placeholder="Uw Telefoonnummer" id="userdetails-phone" name="meta_key[user_phone]" value="<?php echo $userdata[11] ?>">
 										<label for="userdetails-country">Land</label>
-										<input class="u-full-width" type="text" placeholder="Uw land" id="userdetails-country" name="userdetails[country]">
+										<input class="u-full-width" type="text" placeholder="Uw land" id="userdetails-country" name="meta_key[adress_country]" value="<?php echo $userdata[10] ?>">
 								    </div>
 								    <div class="six columns">
 									    <label for="userdetails-adress-*">Adres</label>
-										<input class="u-full-width" type="text" placeholder="Straatnaam 134" id="userdetails-adress-1" name="userdetails[adress][1]">
-								        <input class="u-full-width" type="text" placeholder="Suite/App 1" id="userdetails-adress-2" name="userdetails[adress][2]">
+										<input class="u-full-width" type="text" placeholder="Straatnaam 134" id="userdetails-adress-1" name="meta_key[adress_street]" value="<?php echo $userdata[7] ?>">
 								        <label>&nbsp;</label>
 								        <label for="userdetails-zip">Postcode</label>
-										<input class="u-full-width" type="text" placeholder="Postcode" id="userdetails-zip" name="userdetails[zip]">
+										<input class="u-full-width" type="text" placeholder="Postcode" id="userdetails-zip" name="meta_key[adress_zip]" value="<?php echo $userdata[8] ?>">
 										<label for="userdetails-city">Plaats</label>
-										<input class="u-full-width" type="text" placeholder="Plaatsnaam" id="userdetails-city" name="userdetails[city]">
-
+										<input class="u-full-width" type="text" placeholder="Plaatsnaam" id="userdetails-city" name="meta_key[adress_city]" value="<?php echo $userdata[9] ?>">
 								    </div>
 								  </div>
-								  <input class="button-primary" type="submit" value="Submit">
+								  <input class="button-primary" type="submit" value="Opslaan" name="userdatasubmit">
 			            	</div>
 			            	<div class="element">
 				            	<h5>Betalingsvoorkeuren</h5>
-				            	<label><input checked="checked" type="radio" class="payment-method-radio" id="payment-method-sepa-73441" name="payment[type]" value="manual"><span> Overboeking</span></label>
-				            	<label><input disabled="" type="radio" class="payment-method-radio" id="payment-method-sepa-73441" name="payment[type]" value="sepa"><span> Automatische incasso</span></label>
+				            	<label><input checked="checked" type="radio" class="payment-method-radio" id="payment-method-sepa-73441" name="meta_key[payment_type]" value="manual"><span> Overboeking</span></label>
+				            	<label><input disabled="" type="radio" class="payment-method-radio" id="payment-method-sepa-73441" name="meta_key[payment_type]" value="sepa"><span> Automatische incasso</span></label>
 				            	<p><i>Automatische incasso is op dit moment niet mogelijk voor uw account</i>
 				            	<label for="exampleEmailInput">IBAN</label>
 								<input disabled="" class="u-full-width" type="text" placeholder="Uw naam" id="exampleEmailInput">
 								<label for="exampleEmailInput">Op naam van</label>
 								<input disabled="" class="u-full-width" type="text" placeholder="Uw naam" id="exampleEmailInput">
+								<input class="button-primary" type="submit" value="Opslaan" name="userdatasubmit">
 			            	</div>
 		            	</div>
 		            	<div class="four columns">
 			            	<div class="element">
 				            	<h5>Controlepaneel opties</h5>
-				            	<p>Deze optie is nog niet voor jou beschikbaar</p>
+				            	<p><i>Deze optie is nog niet voor jou beschikbaar</i></p>
+				            	<input class="button-primary" type="submit" value="Opslaan" name="userdatasubmit">
 			            	</div>
 			            	<div class="element">
 				            	<h5>E-mail voorkeuren</h5>
-				            	<p>Deze optie is nog niet voor jou beschikbaar</p>
+				            	<p><i>Deze optie is nog niet voor jou beschikbaar</i></p>
+				            	<input class="button-primary" type="submit" value="Opslaan" name="userdatasubmit">
 			            	</div>
 		            	</div>
+		            	<input hidden="" class="u-full-width" type="text" placeholder="Uw naam" id="userdetails-name" name="id" value="<?php echo $userdata['ID'] ?>">
 	            	</form>
             	</div>
 		    </div>
